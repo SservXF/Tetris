155b8a1da7469f0db16da8e0a567ce077a185aa7
Servan <sservxf@gmail.com>
2023-07-14 17:39:46 +0200

    placement des blocs OK

--- src/main/java/gui/BlocGUI.java
+++ src/main/java/gui/BlocGUI.java
@@ -6,11 +6,21 @@ import main.java.model.Bloc;
 import main.java.model.ColorSwitcher;
 
 import java.awt.*;
+import java.util.Random;
 
 public class BlocGUI extends JPanel {
 
     public BlocGUI(Bloc b){
         setBackground(ColorSwitcher.toColor(b));
+        setOpaque(true);
+    }
+
+    public BlocGUI(){
+        setOpaque(false);
+        /*
+        Random r = new Random();
+        setBackground(ColorSwitcher.toColor(Bloc.values()[r.nextInt(Bloc.values().length)]));
+        */
     }
     
 }
--- src/main/java/gui/BoardGUI.java
+++ src/main/java/gui/BoardGUI.java
@@ -3,6 +3,7 @@ package main.java.gui;
 import javax.swing.JPanel;
 import java.awt.*;
 
+import main.java.model.Bloc;
 import main.java.model.Board;
 
 public class BoardGUI extends JPanel {
@@ -11,21 +12,23 @@ public class BoardGUI extends JPanel {
     protected final int marginLength = 20;
 
     protected Board board;
+    protected BlocGUI[][] grid;
 
     public BoardGUI(int x, int y){
         this.board = new Board(x, y);
+        this.grid = new BlocGUI[board.getLenX()][board.getLenY()];
 
-        setLayout(new GridLayout(board.getLenY(), board.getLenX()));
-        System.out.println(board.getLenX());
-        System.out.println(board.getLenY());
+        setLayout(new GridLayout(board.getLenY(), board.getLenX(),1,1));
 
-        JPanel test = new JPanel();
-        test.setBackground(Color.CYAN);
-        JPanel t = new JPanel();
-        t.setBackground(Color.RED);
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                grid[i][j] = new BlocGUI();
+                this.add(grid[i][j]);
+            }
+        }
 
-        this.add(test);
-        this.add(t);
+        replace(5, 0, new BlocGUI(Bloc.RED));
+
     }
 
     @Override
@@ -34,7 +37,24 @@ public class BoardGUI extends JPanel {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D)g;
 
+        /*
         g2d.setStroke(new BasicStroke(3));
         g2d.drawRect(marginPoint, marginPoint, getWidth()-marginLength, getHeight()-marginLength);
+        */
+    }
+
+    public void replace(int x, int y, BlocGUI bloc){
+        this.removeAll();
+        for (int j = 0; j < board.getLenY(); j++) {
+            for (int i = 0; i < board.getLenX(); i++) {
+                if(i == x && j == y){
+                    grid[x][y] = bloc;
+                }
+                else{
+                    grid[i][j] = new BlocGUI();
+                }
+                this.add(grid[i][j]);
+            }
+        }
     }
 }
