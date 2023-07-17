ee02de8150e975742ec845ad9f77fbd9196eafe9
Servan <sservxf@gmail.com>
2023-07-17 15:28:03 +0200

    Main

--- src/main/java/launcher/Main.java
+++ src/main/java/launcher/Main.java
@@ -2,7 +2,7 @@ package main.java.launcher;
 
 import main.java.gui.WindowTetris;
 
-public class Launcher {
+public class Main {
     public static void main(String[] args) {
         new WindowTetris().start();
     }
